cd6da4fa6c6d92334b648ba4aacdb54a76ada61d
Huong Nguyen <shovity@gmail.com>
2018-01-28 22:19:18 +0700

    fix docs

--- docs.js
+++ docs.js
@@ -1,5 +1,10 @@
+'use strict';
+
 module.exports = 
-`cri [filename] [-p password] [-o output]
-OPTIONS:  -p : password
-          -o : file output
-          -r : replace old file`
+`Usage: cri [filename] [-p password] [-o output] [-r]
+Options:
+  -p      password
+  -o      file output
+  -r      replace old file
+
+Version: 1.1.5`;
