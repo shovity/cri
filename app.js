4a09d6dbb71f9d379c1665486fb8e933d2e2980a
Huong Nguyen <shovity@gmail.com>
2018-01-28 22:03:30 +0700

    fix replace old file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const cipher = require('./lib/cipher');
-
-
 
 module.exports = (params) => {
 
     const inputUri = params.inputUri;
     const decipherMode = inputUri.split('.').slice(-1)[0] === 'cri';
 
-    const outputUri = decipherMode? `${params.outputUri || inputUri.replace('.cri', '')}`
+    const outputUri = decipherMode
+        ? `${params.outputUri || inputUri.replace('.cri', '')}`
         : `${params.outputUri || inputUri}.cri`;
 
     const password = params.password || 'tluafed-terces';
@@ -19,6 +19,12 @@ module.exports = (params) => {
         if (err) {
             console.log(err);
         } else {
+            if (params.replace) {
+                fs.unlink(inputUri, (err) => {
+                  if (err) throw err;
+                  console.log('Replace old file');
+                })
+            }
             console.log(`${decipherMode? 'Deciper' : 'Cipher'} done!`);
         }
     });
